8d5615de95bb6f046501f8480b1fda74a9d434fd
A-Voris <AsherVoris@gmail.com>
2020-04-09 20:01:19 -0600

    Added test files to repo

---
+++ test.c
@@ -0,0 +1,14 @@
+//------------------------------------------------------------------------------
+// @name: test.c
+// @description: Test program for git repo 
+// @last-modified: 2020/04/09
+// @Author: Asher Voris
+//------------------------------------------------------------------------------
+
+#include <stdio.h>
+
+int int main(int argc, char const *argv[])
+{
+	printf("%s\n", "Hello World!");
+	return 0;
+}
